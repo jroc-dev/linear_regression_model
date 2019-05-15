57fe8e7cd92d0235f2418b06e4c5484a1d16f65d
Adam Tesluk <adam.t@bigmirrorlabs.com>
2019-05-15 12:54:47 -0400

    now add our best fit line that we need later

--- linear_regression.py
+++ linear_regression.py
@@ -6,3 +6,14 @@ style.use('seaborn')
 
 x_values = np.array([1,2,3,4,5,6,7,8,9,30], dtype=np.float64)
 y_values = np.array([1,4,1,16,4,1,4,6,10,8], dtype=np.float64)
+
+def best_fit_line(x_values,y_values):
+    m = (((mean(x_values)*mean(y_values)) - mean(x_values*y_values)) /
+         ((mean(x_values)*mean(x_values)) - mean(x_values*x_values)))
+
+    b = mean(y_values) - m*mean(x_values)
+
+    return m, b
+
+# establish best fit line
+m, b = best_fit_line(x_values, y_values)
