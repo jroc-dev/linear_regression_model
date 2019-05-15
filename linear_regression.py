9e653155af1b6c820411eafdc2981aa1d171319f
Adam Tesluk <adam.t@bigmirrorlabs.com>
2019-05-15 12:54:13 -0400

    add arbitrary x and y values

--- linear_regression.py
+++ linear_regression.py
@@ -3,3 +3,6 @@ import matplotlib.pyplot as plt
 from statistics import mean
 from matplotlib import style
 style.use('seaborn')
+
+x_values = np.array([1,2,3,4,5,6,7,8,9,30], dtype=np.float64)
+y_values = np.array([1,4,1,16,4,1,4,6,10,8], dtype=np.float64)
