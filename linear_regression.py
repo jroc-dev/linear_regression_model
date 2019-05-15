7b6ae036f909be6092438b24acc7cb06176b0fde
Adam Tesluk <adam.t@bigmirrorlabs.com>
2019-05-15 12:55:49 -0400

    make the regression line

--- linear_regression.py
+++ linear_regression.py
@@ -7,6 +7,7 @@ style.use('seaborn')
 x_values = np.array([1,2,3,4,5,6,7,8,9,30], dtype=np.float64)
 y_values = np.array([1,4,1,16,4,1,4,6,10,8], dtype=np.float64)
 
+
 def best_fit_line(x_values,y_values):
     m = (((mean(x_values)*mean(y_values)) - mean(x_values*y_values)) /
          ((mean(x_values)*mean(x_values)) - mean(x_values*x_values)))
@@ -15,5 +16,9 @@ def best_fit_line(x_values,y_values):
 
     return m, b
 
+
 # establish best fit line
 m, b = best_fit_line(x_values, y_values)
+
+# establish regression line
+print("regression line: " + "y = " + str(round(m,2)) + "x + " + str(round(b,2)) )
